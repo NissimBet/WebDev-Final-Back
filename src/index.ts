d6295dd1ae326cbc3fdaaea8b9453b979bbeaf66
NissimBet <nissimbet123@outlook.com>
2020-01-28 16:28:25 -0600

    cambio de path de validacion

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/users/register', registerUser);
-app.get('/users/no-email-validate', validateUser);
+app.get('/users/validate', validateUser);
 app.post('/users/login', login);
 
 app.get('/champs/lol/all', getAllLolChampions);
